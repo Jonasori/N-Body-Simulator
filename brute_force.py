372d3bcfd62b6984d3c866da527c3c714a858143
Jonasori <jonasori@gmail.com>
2018-05-01 15:02:13 -0400

    Made almost PEP8 compliant

--- brute_force.py
+++ brute_force.py
@@ -1,3 +1,10 @@
+"""
+Run the brute-force simulation.
+
+blah
+"""
+
+# Import packages
 import copy
 import random
 import time
@@ -5,31 +12,38 @@ import time
 import numpy as np
 from matplotlib import pyplot as plt
 
-from run_nbody_sim import G, AU, mSol, bf, rSol, mEarth, rEarth, dt, \
-    elapsedTimeBH
 from body import body
+from run_nbody_sim import G, AU, mSol, bf, rSol, mEarth, \
+                          rEarth, dt, elapsedTimeBH
 
 
 class systemBF:
+    """Make a class object of the system of bodies."""
     def __init__(self):
-        #self.ax = plt.subplots()                            # not convinced this is necessary
+        """Initialize the class"""
         self.bodies = []                                    # list of bodies
 
-
     def addBod(self, body):
+        """Add a body to the system."""
         self.bodies.append(body)
 
-
     def removeBod(self, body):
+        """Remove a body from the system."""
         self.bodies.remove(body)
 
-
     def EscapeVelocity(self, body):
-        mtot = self.bodies[0].mass
-        vEscape = np.sqrt( 2*G*mtot/d)
+        """Calculate the escape velocity of a given body.
 
+        Not sure what this is actually doing, partially because it's not
+        returning anything and partially because why bodies[0]?
+        Also d should be CoM I assume?
+        This can probably be ignored or removed.
+        """
+        mtot = self.bodies[0].mass
+        vEscape = np.sqrt(2 * G * mtot/d)
 
     def CenterOfMass(self, bodies):
+        """Calculate the center of mass of a group of bodies."""
         mcomx = 0
         mcomy = 0
         mtot = 0
@@ -37,21 +51,36 @@ class systemBF:
             mcomx += i.mass * i.xs[-1]
             mcomy += i.mass * i.ys[-1]
             mtot += i.mass
-        com = np.sqrt( (mcomx/mtot)**2 + (mcomy*mtot)**2 )
+        com = np.sqrt((mcomx / mtot)**2 + (mcomy * mtot)**2)
         return com
 
-
     def collisions(self, body1, body2):
-        d = np.sqrt( (body1.xs[-1] - body2.xs[-1])**2 + (body1.ys[-1] - body2.ys[-1])**2 )
+        """Define how we handle collisions.
+
+        If the distance between two bodies is smaller than the sum of their
+        radii, remove one body, and add its mass and volume to the other.
+        Assume no change in density.
+        """
+        d = np.sqrt((body1.xs[-1] - body2.xs[-1])**2
+                    + (body1.ys[-1] - body2.ys[-1])**2
+                    )
         # Are we colliding?
-        if d < body1.radius + body2.radius:
+        if d < (body1.radius + body2.radius):
+            # Find the density of one body:
+            rho = (3 * self.mass)/(4 * np.pi * (self.radius)**3)
             # Kill one body, add its mass to the other.
-            body1.mass += body2.mass
-            body1.radius = rocheLimit(body1.mass)
+            total_mass = body1.mass + body2.mass
+
+            body1.radius = (3 * (total_mass)/(4 * np.pi * rho))**(1/3)
+            body1.mass = total_mass
             self.removeBod(body2)
 
-
     def plotRV(self, nTimesteps, dt, starIndex):
+        """Plot the RV curve of the central star in a system.
+
+        Indicate which body the star is using the starIndex.
+        Note that this doesn't totally work yet.
+        """
         nTimesteps = nTimesteps/1000
         plt.ion()
         plotVerticalRange = AU
@@ -73,35 +102,36 @@ class systemBF:
                 mtot += i.mass
             comx = mxcomx/mtot
 
-            #ax.plot(t, self.bodies[0].xs[-1], '.y')
-            #ax.plot(t, self.bodies[0].ys[-1], '.r')
-            #ax.axvline(x=t)
-            #ax.plot(t, self.bodies[0].xs[-1] - comx, '.y')
+            ax.plot(t, self.bodies[0].xs[-1], '.y')
+            ax.plot(t, self.bodies[0].ys[-1], '.r')
+            ax.axvline(x=t)
+            ax.plot(t, self.bodies[0].xs[-1] - comx, '.y')
             ax.plot(t, self.bodies[0].velocity[0], '.y')
             plt.show(block=False)
             plt.pause(0.0001)
 
-
     def step_BruteForce(self, dt):
-        # To be sure that we're not taking updated positions, copy all the bodies' current positions and analyze from that.
+        """Step the system forward through time.
+
+        To be sure that we're not taking updated positions, begin by copying
+        all the bodies' current positions and calculate steps from that.
+        """
         oldBods = copy.deepcopy(self.bodies)
         for i in self.bodies:
-            #print "\nCurrently Brute-Force stepping ", i.name
-            # Calculate the gravitaional acceleration due to all other bodies on i
+            # Calculate the grav. accel. due to all other bodies on i
             for ob in oldBods:
                 i.aGrav(ob)
             # Now update the positions and velocities.
             i.positionAndVelocityUpdater(dt)
 
-
-            # If a body is too far out of the FOV, just get rid of it.
-            # Removing this for the time being just so no bodies get lost -> more accurate timing
+            # If a body is too far out of the FOV, get rid of it.
+            # Removing this for the time being just so no bodies get lost
+            # -> more accurate timing
             """
             if abs(np.sqrt( (i.xs[-1])**2 + (i.ys[-1])**2)) > 1.5*axlims:
                 self.removeBod(i)
                 print "removed", i.name, "for being too far away"
                 """
-
 
     def run_BruteForce(self, nTimesteps, nSmallBods):
         # Start the timer:
@@ -109,48 +139,41 @@ class systemBF:
         mStar = mSol
         # exampleBody =  body(name, mass, radius, xy, velocity, color)
         bf.addBod(body('Star', mStar, rSol, [0,0], [0,-0], 'y'))
-        #bf.addBod(body('Mercury', 0.055*mEarth, 0.3829*rEarth, [-0.4481*AU, 0], [0, -55410], 'blue'))
+        # bf.addBod(body('Mercury', 0.055*mEarth, 0.3829*rEarth, [-0.4481*AU, 0], [0, -55410], 'blue'))
         bf.addBod(body('Venus', 0.815*mEarth, 0.949*rEarth, [0.721*AU, 0], [0, 34910], 'orange'))
         bf.addBod(body('Earth', mEarth, rEarth, [0, AU], [-29838, -0], 'g'))
-        #bf.addBod(body('Mars', 0.10745*mEarth, 0.531*rEarth, [0, -1.52*AU], [240740, 0], 'red'))
-        #bf.addBod(body('Jupiter', 317*mEarth, 11*rEarth, [5.2*AU, 0], [0, 13048], 'magenta'))
-        #bf.addBod(body('FastJupiter', 317*mEarth, 11*rEarth, [5.2*AU, 0], [0, 40000], 'magenta'))
-        #bf.addBod(body('Saturn', 95.16*mEarth, 9.14*rEarth, [0,10.06*AU], [-10180, 0], 'orange'))
-        #bf.addBod(body('Uranus', 14.53*mEarth, 3.976*rEarth, [-19.91*AU, 0], [0, -7058], 'blue'))
-        #bf.addBod(body('Neptune', 17.148*mEarth, 3.86*rEarth, [0, -29.95*AU], [5413, 0], 'cyan'))
+        # bf.addBod(body('Mars', 0.10745*mEarth, 0.531*rEarth, [0, -1.52*AU], [240740, 0], 'red'))
+        # bf.addBod(body('Jupiter', 317*mEarth, 11*rEarth, [5.2*AU, 0], [0, 13048], 'magenta'))
+        # bf.addBod(body('FastJupiter', 317*mEarth, 11*rEarth, [5.2*AU, 0], [0, 40000], 'magenta'))
+        # bf.addBod(body('Saturn', 95.16*mEarth, 9.14*rEarth, [0,10.06*AU], [-10180, 0], 'orange'))
+        # bf.addBod(body('Uranus', 14.53*mEarth, 3.976*rEarth, [-19.91*AU, 0], [0, -7058], 'blue'))
+        # bf.addBod(body('Neptune', 17.148*mEarth, 3.86*rEarth, [0, -29.95*AU], [5413, 0], 'cyan'))
 
-        # V2434 Ori
-        #bf.addBod(body('V2434a', 3.5*mSol, 3.5*rSol, [-220*AU, 0], [0, -np.sqrt((-G*6*mSol)/(220*AU))], 'blue'))
-        #bf.addBod(body('V2434b', 3.*mSol, 3*rSol, [220*AU, 0], [0, np.sqrt((-G*6*mSol)/(220*AU))], 'cyan'))
-        #"""
-        # Inner ring of small random bodies:
+        # Simualte my research target stars, V2434 Ori
+        # bf.addBod(body('V2434a', 3.5*mSol, 3.5*rSol, [-220*AU, 0], [0, -np.sqrt((-G*6*mSol)/(220*AU))], 'blue'))
+        # bf.addBod(body('V2434b', 3.*mSol, 3*rSol, [220*AU, 0], [0, np.sqrt((-G*6*mSol)/(220*AU))], 'cyan'))
+
+        # Make an inner ring of small random bodies:
         if nSmallBods > 0:
+            # Change these if you want
+            m = 0.05*mEarth
+            r = 0.02*rEarth
             for i in range(0, nSmallBods):
                 print "Adding small bod!", i
                 r = random.uniform(4.9*AU, 5.1*AU)
-                x = random.uniform(0,r)
+                x = random.uniform(-r, r)
                 y = np.sqrt(r**2 - x**2)
 
-                # velocity direction is the sign of the quadrant times the sign of the coordinate (x or y)
-
-                # THIS IS INVERTED RELATIVE TO THE BF VERSION
-                v = np.sqrt(r*AU/(-G*mStar))
-
-                """
-                s = (x*y)/abs(x*y)
-                # Add some random directions
-                variance = random.uniform(-1000, 1000)
-                vx = variance * v * s * (x/abs(x))
-                vy = variance * v * s * (y/abs(y))
-                """
+                # Velocity direction is the sign of the quadrant
+                # times the sign of the coordinate (x or y)
+                v = np.sqrt((G * mStar)/(r * AU))
 
                 variance = random.uniform(-1000, 1000)
                 vx = -v * (y/abs(y)) * np.sin(x/y) + variance
                 vy = v * (x/abs(x)) * np.cos(x/y) + variance
 
-                bf.addBod(body('planetesimal_'+str(i), 0.05*mEarth, 0.02*rEarth, [x, y], [vx, vy], 'y'))
-
-
+                name = 'planetesimal_'+str(i)
+                bf.addBod(body(name, m, r, [x, y], [vx, vy], 'y'))
 
         # Make a file that has the filename of each body, initiate body files.
         fnames = []
@@ -185,16 +208,3 @@ class systemBF:
         print "Final Duration with", nSmallBods, "small bodies over", nTimesteps, "steps (seconds):", time.time() - startBF
         #plotTrajectories()
         return [nSmallBods, time.time() - startBF]
-
-
-
-
-
-
-
-
-
-
-
-
-
